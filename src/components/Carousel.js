4620b6f31d4d92ec4e1e6d58e1721008a3a7b0f8
Krzysztof Broniszewski <k.broniszewski@gmail.com>
2024-07-13 15:48:09 +0200

    arrows right and left added

---
+++ src/components/Carousel.js
@@ -0,0 +1,47 @@
+import React, { useState } from 'react';
+import "../style/Carousel.css";
+// import IMAGES from './Data.js';
+
+
+
+function Carousel({ images }) {
+  const counter = images.length - 1;
+  const [current, setCurrent] = useState(0);
+
+  const slideRight = () => {
+    const newCurrent = current + 1;
+    setCurrent(newCurrent < counter + 1 ? newCurrent : 0);
+  };
+
+  const slideLeft = () => {
+    const newCurrent = current - 1;
+    setCurrent(newCurrent < 0 ? counter : newCurrent);
+  };
+
+  return (
+    <div className="carousel">
+      {/* {IMAGES && IMAGES.map((item) =>
+        <div key={item.id}>
+          <img className='img' src={item.path} alt={item.title} />
+        </div>
+      )
+      } */}
+      <div className="carousel_wrapper">
+        {images.map((image, index) => {
+          return (
+            <div key={index} className={index == current ? "carousel_card carousel_card-active" : "carousel_card"}>
+              <img className="img" src={image.path} alt={image.title} />
+              <div className="card_overlay">
+                <h2 className="card_title">{index + 1}/{counter+1} - {image.title}</h2>
+              </div>
+            </div>
+          );  
+        })}
+        <div className="carousel_arrow_left" onClick={slideLeft}>&lsaquo;</div>
+        <div className="carousel_arrow_right" onClick={slideRight}>&rsaquo;</div>
+      </div>
+    </div>
+  );
+}
+
+export default Carousel
---
+++ src/components/Data.js
@@ -0,0 +1,74 @@
+// const IMAGES = [
+//   {
+//     id: 1,
+//     path: 'images/2006_01.jpg',
+//     desc: "image01",
+//   },
+//   {
+//     id: 2,
+//     path: "images/2006_02.jpg",
+//     desc: "image02",
+//   },
+//   {
+//     id: 3,
+//     path: "images/2006_03.jpg",
+//     desc: "image03",
+//   },
+//   {
+//     id: 4,
+//     path: "images/2006_04.jpg",
+//     desc: "image04",
+//   },
+//   {
+//     id: 5,
+//     path: "images/2006_05.jpg",
+//     desc: "image05",
+//   },
+// ];
+
+const IMAGES = [
+  {
+    id: 1,
+    title: "First Img",
+    desc: "Las",
+    path:
+      "https://fastly.picsum.photos/id/28/4928/3264.jpg?hmac=GnYF-RnBUg44PFfU5pcw_Qs0ReOyStdnZ8MtQWJqTfA",
+  },
+  {
+    id: 2,
+    title: "Second Img",
+    desc: "Skały",
+    path:
+      "https://fastly.picsum.photos/id/16/2500/1667.jpg?hmac=uAkZwYc5phCRNFTrV_prJ_0rP0EdwJaZ4ctje2bY7aE",
+  },
+  {
+    id: 3,
+    title: "Third Img",
+    desc: "Plaża",
+    path:
+      "https://fastly.picsum.photos/id/13/2500/1667.jpg?hmac=SoX9UoHhN8HyklRA4A3vcCWJMVtiBXUg0W4ljWTor7s",
+  },
+  {
+    id: 4,
+    title: "Fourth Img",
+    desc: "Góry",
+    path:
+      "https://fastly.picsum.photos/id/29/4000/2670.jpg?hmac=rCbRAl24FzrSzwlR5tL-Aqzyu5tX_PA95VJtnUXegGU",
+  },
+  {
+    id: 5,
+    title: "Fifth Img",
+    desc: "Nosek",
+    path:
+      "https://fastly.picsum.photos/id/40/4106/2806.jpg?hmac=MY3ra98ut044LaWPEKwZowgydHZ_rZZUuOHrc3mL5mI",
+  },
+  {
+    id: 6,
+    title: "Six Img",
+    desc: "Most",
+    path:
+      "https://fastly.picsum.photos/id/84/1280/848.jpg?hmac=YFRYDI4UsfbeTzI8ZakNOR98wVU7a-9a2tGF542539s",
+  },
+];
+
+export default IMAGES;
